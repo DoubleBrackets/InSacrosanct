56417daee08093280478c77dd00a00229b099395
RonnocJ <ronnochj@gmail.com>
2024-10-20 16:23:40 -0700

    Fixed Death problem

--- InSacrosanct/Assets/Scripts/Utility/MusicPlayer.cs
+++ InSacrosanct/Assets/Scripts/Utility/MusicPlayer.cs
@@ -4,9 +4,12 @@ public class MusicPlayer : MonoBehaviour
 {
     [SerializeField]
     private AudioManager.Tracks _track;
+    [SerializeField]
+    private AK.Wwise.Bank _bank;
 
     private void Start()
     {
+        _bank.Load();
         AudioManager.Instance.PlayMusic(_track);
     }
 }
